bfd10ec7b05800d331412624003747180dc005d7
Leo Wong <leoelicos@gmail.com>
2022-07-07 20:58:48 +1000

    Use style.css to better organise React code

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import '../styles/style.css';
 
 const Project = ({ title, description, deployedURL, repoURL, imageURL }) => {
   return (
-    <li>
-      <div background={imageURL}>
-        {title}
-        <br />
-        {description}
-        <br />
-        <a href={deployedURL} target="_blank" rel="noreferrer">
-          Deployed
+    <section className="content-wrapper">
+      <section className="content">
+        <h2>{title}</h2>
+        <h3>{description}</h3>
+
+        <div className="code-links">
+          <a href={repoURL} target="_blank" rel="noreferrer" className="repo">
+            🧩code
+          </a>
+          <a href={deployedURL} target="_blank" rel="noreferrer" className="deployed">
+            ❇️ app
+          </a>
+        </div>
+        <a className="anchor-past-project" target="_blank" rel="noreferrer" href={deployedURL}>
+          <img className="content-image" src={imageURL} alt="Kiddle game" />
         </a>
-        <a href={repoURL} target="_blank" rel="noreferrer">
-          GitHub Repo
-        </a>
-      </div>
-    </li>
+      </section>
+    </section>
   );
 };
 
