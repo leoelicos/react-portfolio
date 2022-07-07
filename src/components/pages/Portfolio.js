59a886c737239935a752b30cd39e8c00b91f4544
Leo Wong <leoelicos@gmail.com>
2022-07-07 17:42:24 +1000

    Portfolio component

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>My Applications</h1>
+      <ol>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+        <li>
+          App <a href="/">Deployed</a> <a href="/">Repo</a>
+        </li>
+      </ol>
+    </div>
+  );
+}
