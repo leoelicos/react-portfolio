5fe430163dd9b2e51bf5f43884288e122a6f874c
Leo Wong <leoelicos@gmail.com>
2022-07-24 21:09:43 +1000

    Add bio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,24 +6,30 @@ export default function About() {
       <img className="avatar" src="https://leoelicos.github.io/bcs-02-professional-portfolio/assets/images/avatar.jpg" alt="leo wong" />
 
       <section className="content d-flex flex-column align-items-center">
-        <h3>Full Stack Developer</h3>
+        <h4>Full Stack Dev</h4>
       </section>
 
       <section className="content d-flex flex-column align-items-center">
         <h3>My process</h3>
-        <h3>
+        <h4>
           <span className="hashtag">#TDD</span>
           <span className="hashtag">#DRY</span>
           <span className="hashtag">#agile</span>
-        </h3>
+        </h4>
       </section>
       <section className="content d-flex flex-column align-items-center">
         <h3>My values</h3>
-        <h3>
+        <h4>
           <span className="hashtag">#original</span>
           <span className="hashtag">#teamwork</span>
           <span className="hashtag">#bestpractice</span>
-        </h3>
+        </h4>
+      </section>
+      <section className="content d-flex flex-column align-items-center">
+        <h3>Bio</h3>
+        <h4>
+          <span className="hashtag">Hello, I'm Leo. I'm currently studying to become a full-stack developer. I spend my days at bootcamp, on codewars and on leetcode. Contact me at leoelicos at gmail dot com</span>
+        </h4>
       </section>
     </article>
   );
